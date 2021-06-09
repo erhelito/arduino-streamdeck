e91fe99aeee951cb1ef7d021c13a8f643000eee9
erhelito <erhelito@yahoo.fr>
2021-06-09 10:44:29 +0200

    ajout de commentaires sur le fichier python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.8
 # -*-coding:Utf-8 -*
 
+# importation des modules nécessaires dans ce programme
 import os
 import serial
 import time
@@ -11,12 +12,13 @@ reste à faire :
 - couper micro
 - volume micro
 """
-
+# connection au port série
 port = "/dev/ttyACM0"
-
 connection = False
 
+# définition des différentes fonctions
 def recup_et_nettoyage_donnees() :
+    # récupère les données brutes, les affiche, et supprime les informations inutiles
     donnees_brutes = str(arduino.readline())
     print(donnees_brutes)
     donnees = donnees_brutes[2:-5]
@@ -25,6 +27,7 @@ def recup_et_nettoyage_donnees() :
     return donnees
 
 def analyse_donnees(donnees) :
+    # partitionne les données pour les utiliser dans le programme, et les affiche
     mode = int(donnees[0:1])
     b1 = int(donnees[1:2])
     b2 = int(donnees[2:3])
@@ -35,9 +38,13 @@ def analyse_donnees(donnees) :
     return mode, b1, b2, b3, potentiometre
 
 def mode1(b1, b2, b3) :
+    # cette fonction est celle pour spotify
+
+    # met le volume à la valeur du potentiomètre
     commande_son = "amixer set Master {0}%".format(potentiometre)
     os.system(commande_son)
 
+    # pause/lecture/suivant/precedent en fonction des différentes valeurs des boutons
     if b1 == 1 :
         os.system("pytify -pp")
         time.sleep(1)
@@ -53,7 +60,9 @@ def mode1(b1, b2, b3) :
         time.sleep(1)
         b3 = 0
 
+# boucle
 while True :
+    # connection au port série (uniquement lorsque le port série n'était pas connecté, afin de faire un programme plus léger à executer)
     while connection == False :
         try :
             arduino = serial.Serial(port, timeout = 1)
@@ -64,6 +73,7 @@ while True :
             connection = False
             
     try :
+        # récupération de analyse des données
         donnees = recup_et_nettoyage_donnees()
 
         mode = analyse_donnees(donnees)[0]
@@ -72,6 +82,7 @@ while True :
         b3 = analyse_donnees(donnees)[3]
         potentiometre = analyse_donnees()[4]
 
+        # appelle la fonction du mode 1 si c'est le mode qui est sélectionné
         if mode == 1 :
             mode1(b1, b2, b3)
             

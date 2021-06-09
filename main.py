85e837ba790411a1afc83f4a744322618265b6ec
erhelito <erhelito@yahoo.fr>
2021-06-09 10:35:08 +0200

    ajout des modes sur le fichier python. Le mode 1 est programmé, il manque à faire les 2 autres

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ reste à faire :
 - couper volume
 - couper micro
 - volume micro
-- améliorer la liaison série (dans le programme arduino)
 """
 
 port = "/dev/ttyACM0"
@@ -25,14 +24,34 @@ def recup_et_nettoyage_donnees() :
 
     return donnees
 
-def analyse_donnees() :
-    pause = int(donnees[0:1])
-    suivant = int(donnees[1:2])
-    precedent = int(donnees[2:3])
-    volume = int(donnees[3:])
-    print(pause, suivant, precedent, volume)
+def analyse_donnees(donnees) :
+    mode = int(donnees[0:1])
+    b1 = int(donnees[1:2])
+    b2 = int(donnees[2:3])
+    b3 = int(donnees[3:4])
+    potentiometre = int(donnees[4:7])
+    print(mode, b1, b2, b3, potentiometre)
 
-    return pause, suivant, precedent, volume
+    return mode, b1, b2, b3, potentiometre
+
+def mode1(b1, b2, b3) :
+    commande_son = "amixer set Master {0}%".format(potentiometre)
+    os.system(commande_son)
+
+    if b1 == 1 :
+        os.system("pytify -pp")
+        time.sleep(1)
+        b1 = 0
+                
+    if b2 == 1 :
+        os.system("pytify -n")
+        time.sleep(1)
+        b2 = 0
+                
+    if b3 == 1 :
+        os.system("pytify -p")
+        time.sleep(1)
+        b3 = 0
 
 while True :
     while connection == False :
@@ -44,33 +63,18 @@ while True :
             print("EN: An error occurred. If it happens again, check the serial port above.")
             connection = False
             
-    try :        
+    try :
         donnees = recup_et_nettoyage_donnees()
-        
-        if len(donnees) > 3 :
-            pause = analyse_donnees()[0]
-            suivant = analyse_donnees()[1]
-            precedent = analyse_donnees()[2]
-            volume = analyse_donnees()[3]
 
-            commande_son = "amixer set Master {0}%".format(volume)
-            os.system(commande_son)
-          
-            if pause == 1 :
-                os.system("pytify -pp")
-                time.sleep(1)
-                pause = 0
-                
-            if suivant == 1 :
-                os.system("pytify -n")
-                time.sleep(1)
-                suivant = 0
-               
-            if precedent == 1 :
-                os.system("pytify -p")
-                time.sleep(1)
-                precedent = 0
+        mode = analyse_donnees(donnees)[0]
+        b1 = analyse_donnees(donnees)[1]
+        b2 = analyse_donnees(donnees)[2]
+        b3 = analyse_donnees(donnees)[3]
+        potentiometre = analyse_donnees()[4]
 
+        if mode == 1 :
+            mode1(b1, b2, b3)
+            
     except :
         print("FR : Un problème est survenu, il peut s'agir d'une erreur lors du traitement des données. Si cette erreur apparaît à nouveau, vous pouvez contacter Erhelito (erhelito@yahoo.fr).")
         print("EN: An error occurred. If it happens again, contact Erhelito (erhelito@yahoo.fr).")

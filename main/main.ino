f12043d947e1d44edf15d7d3820497206c1e43e0
erhelito <erhelito@yahoo.fr>
2021-06-09 09:48:31 +0200

    ajout de commentaires sur le programme arduino

--- main/main.ino
+++ main/main.ino
@@ -1,39 +1,46 @@
-int pause;
-int suivant;
-int precedent;
+// initialisation des valeurs des trois boutons, et des broches sur lesquels ils sont branchés
+int b1;
+int b2;
+int b3;
 
+const int b1Pin = 3;
+const int b2Pin = 2;
+const int b3Pin = 4;
+
+// initialisation des valeurs du potentiomètre, et de ses broches
 int val_potentiometre;
-int volume;
-
-const int pausePin = 3;
-const int suivantPin = 2;
-const int precedentPin = 4;
+int pourcentage;
 
 const int potentiometrePin = A0;
 
 void setup() {
-    pinMode(pausePin, INPUT);
-    pinMode(suivantPin, INPUT);
-    pinMode(precedentPin, INPUT);
+    // déclaration des inputs et des outputs, et début de la liaison série
+    pinMode(b1Pin, INPUT);
+    pinMode(b2Pin, INPUT);
+    pinMode(b3Pin, INPUT);
 
     Serial.begin(9600);
 }
 
 void loop() {
+    // appel des fonctions boutons de pourcentage (pour traiter les données des différents boutons et du potentiomètre)
     boutons();
-    fonct_volume();
+    fonct_pourcentage();
 
+    // nettoyage du port série (obligatoire pour  un traitement depuis le programme python)
     Serial.flush();
 
+    // delay pour ne pas surcharger le port série
     delay(100);
 }
 
 void boutons() {
-    pause = digitalRead(pausePin);
-    suivant = digitalRead(suivantPin);
-    precedent = digitalRead(precedentPin);
+    // lecture des valeurs des différents boutons, et affichage dans le port série
+    b1 = digitalRead(b1Pin);
+    b2 = digitalRead(b2Pin);
+    b3 = digitalRead(b3Pin);
 
-    if (pause == 1) {        
+    if (b1 == 1) {        
         Serial.print(1);
     }
 
@@ -42,7 +49,7 @@ void boutons() {
     }
 
 
-    if (suivant == 1) {        
+    if (b2 == 1) {        
         Serial.print(1);
     }
 
@@ -51,7 +58,7 @@ void boutons() {
     }
 
 
-    if (precedent == 1) {        
+    if (b3 == 1) {        
         Serial.print(1);
     }
 
@@ -60,22 +67,26 @@ void boutons() {
     }
 }
 
-void fonct_volume() {
-    volume = analogRead(potentiometrePin);
-    volume = map(volume, 0, 1023, 0, 100);
+void fonct_pourcentage() {
+    // lecture de la valeur brute du potentiomètre
+    pourcentage = analogRead(potentiometrePin);
+    // passage en pourcentage (la fonction map fait un produit en croix)
+    pourcentage = map(pourcentage, 0, 1023, 0, 100);
 
-    if (volume < 10) {
+    // traitement des données du potentiomètre pour un meilleur affichage dans le port série
+    // (affiche par exemple 004 au lieu de 4, ce qui empêche les bugs dans le traitement depuis le programme python)
+    if (pourcentage < 10) {
         Serial.print("0");
         Serial.print("0");
-        Serial.println(volume);
+        Serial.println(pourcentage);
     }
 
-    else if (volume < 100) {
+    else if (pourcentage < 100) {
         Serial.print("0");
-        Serial.println(volume);
+        Serial.println(pourcentage);
     }
 
     else {
-        Serial.println(volume);
+        Serial.println(pourcentage);
     }
 }
